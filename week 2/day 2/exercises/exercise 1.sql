df77b2b58be8163f03c79a80c0621561e8e2643e
elfajjaj <fatimaezzahra.elfajjaj@gmail.com>
2025-08-20 22:51:35 +0100

    update

---
+++ week 2/day 2/exercises/exercise 1.sql
@@ -0,0 +1,32 @@
+select * from items 
+order by price;
+
+select * from items 
+where price >= 80 
+order by price desc;
+
+
+select first_name,last_name from customers
+order by first_name 
+limit 3 ;
+
+
+select last_name from customers
+order by last_name desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
