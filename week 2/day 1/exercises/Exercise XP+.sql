e64368e73f8bd9e3d2dacdd36a0fb53cadb9fda5
elfajjaj <fatimaezzahra.elfajjaj@gmail.com>
2025-08-20 13:55:17 +0100

    psql

--- week 2/day 1/exercises/Exercise XP+.sql
+++ week 2/day 1/exercises/Exercise XP+.sql
@@ -1,28 +1,28 @@
--- create table students(
--- id serial primary key,
--- first_name varchar (50),
--- last_name varchar (50),
--- birth_date date 
--- );
+create table students(
+id serial primary key,
+first_name varchar (50),
+last_name varchar (50),
+birth_date date 
+);
 
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('Marc', 'Benichou', '1998-11-02');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('Marc', 'Benichou', '1998-11-02');
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('Yoan', 'Cohen', '2010-12-03');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('Yoan', 'Cohen', '2010-12-03');
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('Lea', 'Benichou', '1987-07-27');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('Lea', 'Benichou', '1987-07-27');
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('Amelia', 'Dux', '1996-04-07');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('Amelia', 'Dux', '1996-04-07');
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('David', 'Grez', '2003-06-14');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('David', 'Grez', '2003-06-14');
 
--- INSERT INTO students (first_name, last_name, birth_date) 
--- VALUES ('Omer', 'Simpson', '1980-10-03');
+INSERT INTO students (first_name, last_name, birth_date) 
+VALUES ('Omer', 'Simpson', '1980-10-03');
 
 
 
---
+++ week 2/day 1/exercises/Exrecises XP Gold.sql
@@ -0,0 +1,24 @@
+select first_name,last_name, birth_date 
+from students 
+order by last_name  
+limit 4 ;
+
+select first_name,last_name, birth_date
+from students
+order by birth_date desc
+limit 1;
+
+select first_name,last_name, birth_date
+from students
+limit 3
+offset 2;
+
+
+
+
+
+
+
+
+
+
