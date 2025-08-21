9343d1e14128eee75455fd459ebc97702aab8b55
elfajjaj <fatimaezzahra.elfajjaj@gmail.com>
2025-08-21 10:20:01 +0100

    update

---
+++ week 2/day 2/challenge/Puzzle.sql
@@ -0,0 +1,18 @@
+-- Q1
+0
+-- Because the subquery contains NULL, the NOT IN condition fails for all rows
+
+
+-- Q2
+2
+-- The condition only excludes id = 5, so the other rows are counted.
+
+
+-- Q3
+0
+-- The subquery returns 5 and NULL. Because of NULL, NOT IN excludes everything
+
+
+-- Q4
+2
+-- The subquery returns only 5, so only id = 5 is excluded
