58c3f89dbee9bfb7c35d4dff786c8017b4da317a
elfajjaj <fatimaezzahra.elfajjaj@gmail.com>
2025-10-13 13:11:52 +0100

    save local changes before rebase

--- week7/day3/Exercises_XP/exercisesxp/src/App.js
+++ week7/day3/Exercises_XP/exercisesxp/src/App.js
@@ -1,108 +1,108 @@
-// Exercise 1
+// // Exercise 1
 
-import React from "react";
-import ErrorBoundary from "./Components/ErrorBoundary";
+// import React from "react";
+// import ErrorBoundary from "./Components/ErrorBoundary";
 
-class BuggyCounter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-  }
+// class BuggyCounter extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { counter: 0 };
+//   }
 
-  handleClick = () => {
-    this.setState(({ counter }) => ({ counter: counter + 1 }));
-  };
+//   handleClick = () => {
+//     this.setState(({ counter }) => ({ counter: counter + 1 }));
+//   };
 
-  render() {
-    if (this.state.counter === 5) {
-      throw new Error("I crashed!");
-    }
-    return <h2 onClick={this.handleClick}>Counter: {this.state.counter}</h2>;
-  }
-}
+//   render() {
+//     if (this.state.counter === 5) {
+//       throw new Error("I crashed!");
+//     }
+//     return <h2 onClick={this.handleClick}>Counter: {this.state.counter}</h2>;
+//   }
+// }
 
 
-function App() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "40px" }}>
-      <h1>React Error Boundary Demo</h1>
+// function App() {
+//   return (
+//     <div style={{ textAlign: "center", marginTop: "40px" }}>
+//       <h1>React Error Boundary Demo</h1>
 
-      {/* Simulation 1:*/}
-      <h3>Simulation 1: One ErrorBoundary for both</h3>
-      <ErrorBoundary>
-        <BuggyCounter />
-        <BuggyCounter />
-      </ErrorBoundary>
+//       {/* Simulation 1:*/}
+//       <h3>Simulation 1: One ErrorBoundary for both</h3>
+//       <ErrorBoundary>
+//         <BuggyCounter />
+//         <BuggyCounter />
+//       </ErrorBoundary>
 
-      <hr />
+//       <hr />
 
-      {/* Simulation 2:*/}
-      <h3>Simulation 2: Each one has its own ErrorBoundary</h3>
-      <ErrorBoundary>
-        <BuggyCounter />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <BuggyCounter />
-      </ErrorBoundary>
+//       {/* Simulation 2:*/}
+//       <h3>Simulation 2: Each one has its own ErrorBoundary</h3>
+//       <ErrorBoundary>
+//         <BuggyCounter />
+//       </ErrorBoundary>
+//       <ErrorBoundary>
+//         <BuggyCounter />
+//       </ErrorBoundary>
 
-      <hr />
+//       <hr />
 
-      {/* Simulation 3:*/}
-      <h3>Simulation 3: No ErrorBoundary (will crash the app)</h3>
-      <BuggyCounter />
-    </div>
-  );
-}
+//       {/* Simulation 3:*/}
+//       <h3>Simulation 3: No ErrorBoundary (will crash the app)</h3>
+//       <BuggyCounter />
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
 
-// Exercise 2 : Lifecycle
+// // Exercise 2 : Lifecycle
 
-import React from "react";
+// import React from "react";
 
-class Color extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { favoriteColor: "red" };
-  }
+// class Color extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { favoriteColor: "red" };
+//   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ favoriteColor: "yellow" });
-    }, 2000);
-  }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ favoriteColor: "yellow" });
+//     }, 2000);
+//   }
 
-  // Part I:
-  shouldComponentUpdate() {
-    return true; 
-  }
+//   // Part I:
+//   shouldComponentUpdate() {
+//     return true; 
+//   }
 
-  // Part III: 
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log("in getSnapshotBeforeUpdate");
-    return null;
-  }
+//   // Part III: 
+//   getSnapshotBeforeUpdate(prevProps, prevState) {
+//     console.log("in getSnapshotBeforeUpdate");
+//     return null;
+//   }
 
-  // Part II: 
-  componentDidUpdate() {
-    console.log("after update");
-  }
+//   // Part II: 
+//   componentDidUpdate() {
+//     console.log("after update");
+//   }
 
-  changeColor = () => {
-    this.setState({ favoriteColor: "blue" });
-  };
+//   changeColor = () => {
+//     this.setState({ favoriteColor: "blue" });
+//   };
 
-  render() {
-    return (
-      <div style={{ textAlign: "center", marginTop: "30px" }}>
-        <h1>My Favorite Color is {this.state.favoriteColor}</h1>
-        <button onClick={this.changeColor}>Change to blue</button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div style={{ textAlign: "center", marginTop: "30px" }}>
+//         <h1>My Favorite Color is {this.state.favoriteColor}</h1>
+//         <button onClick={this.changeColor}>Change to blue</button>
+//       </div>
+//     );
+//   }
+// }
 
-export default Color;
+// export default Color;
 
 
 // Exercise 3 : Lifecycle #2
